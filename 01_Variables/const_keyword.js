5f5f65345fdeb0de944f38493237a2b22ae0ac69
Muhammad Musanif <muhammadmusanif7292@gmail.com>
2025-07-28 02:43:35 +0500

    use of const keyword and its scope

---
+++ 01_Variables/const_keyword.js
@@ -0,0 +1,11 @@
+// CONST keyword
+//once a varaible is decleared it can't be re-decleared
+
+
+const a= 30;
+// const a = 40; will give syntax error
+
+
+const variable = 50;
+const age= 20;
+const studentName = "muhammad Musanif";  
