02937adab1449eaccefa0ae171a34347fb31aec7
Muhammad Musanif <muhammadmusanif7292@gmail.com>
2025-07-28 02:45:34 +0500

    Let keyword - understanding and scope

---
+++ 01_Variables/let_keyword_practice.js
@@ -0,0 +1,25 @@
+// here we will practice the let keyword
+// let keyword has a block level scop "Means work in block"
+
+// let a=2; //declearing a variable a using let keyword output is 2
+// let b="I am learning java Script keyword"; // I am learning java Script keyword
+// let c='q'; // output is q , excessing corrector using letkeyword
+// let d='Muhammad'; // Muhammad , this will give us a string
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// Usinf letkeyword in 
+
+   function student() // creating funstion student
+    { 
+          let a= 'Muhammad'; 
+        if (a='Muhammad');{
+    let test='Musanif'; // 
+    console.log(test) // output is Musanif beacuse of the function call
+    }
+    
+}
+student();
