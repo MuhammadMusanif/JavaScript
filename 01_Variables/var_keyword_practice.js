fab1d5730567eadba4f5d8cf8e8dc17cba12ac06
Muhammad Musanif <muhammadmusanif7292@gmail.com>
2025-07-28 02:56:46 +0500

    practicing var keyword and its scope also upploaded to github

---
+++ 01_Variables/var_keyword_practice.js
@@ -0,0 +1,48 @@
+// var keyword is a function level scope
+
+
+// declearing a simple var keyword like constant num, char, string, float etc.
+// var a=2;
+// var b='M';
+// var c="Muhammad";
+// var d=12.34;
+// var learning="I am learning javascript"
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(learning);
+
+
+
+
+// var keyword using function
+
+// function student(){
+//     var name= "Muhammad";{
+//     var age= 20;
+//     var registrationNumber= "22pwbcs0927";
+// }
+
+// console.console.log(age);
+//console.log(registrationNumber);
+// console.log(age);
+
+// }
+// student();
+
+
+
+// declearing a function with the name Bank
+
+function bank(){
+    var name="Habbib bank limited";{
+    var manager="Malik Majeed";
+    var cashier="Ameer hamza, Sudais Qayoom";
+}
+    console.log(name);
+    console.log(manager);
+    console.log(cashier);
+}
+bank();
