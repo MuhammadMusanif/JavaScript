7415a123eadec072a47d2f88843ae379317e241a
Muhammad Musanif <muhammadmusanif7292@gmail.com>
2025-08-02 14:45:32 +0500

    Arrays in Java-Script

---
+++ 03_Arrays/array_declearation.js
@@ -0,0 +1,10 @@
+// Array in java script means order, means that elements/attributes are stord in a sequence.
+
+// const student=['Muhammad','22pwbcs0927','cs & it', 'section B']
+
+// console.log(student)
+
+const Fruits=['apple', 'banana', 'orange']
+// console.log(fruits)
+
+Fruits.forEach((fruit)=>{ console.log(fruit)})
---
+++ 03_Arrays/arrays_indexing.js
@@ -0,0 +1,45 @@
+// Arrays are zero indexed means the element of the array are it zero indexed.
+
+
+// Array indexing for Students
+// const students =['Muhammad', 'Musanif','cs & it', '22pwbcs0927'];
+//  const firststudent = students[0]; // Muhammad
+//  const secondstudent = students[1]; // Musanif
+//  const thirdstudent = students[2]; // cs & it
+//  const fourthstudent = students[3]; // 22pwbcs0927
+
+//  console.log(firststudent);
+//  console.log(secondstudent);
+//  console.log(thirdstudent);
+//  console.log(fourthstudent);
+
+
+//Array indexing for fruits
+//  const fruits =['Apple', 'Banana','Mango', 'Orange'];
+//  const firstfruit = fruits[0]; // Apple
+//  const secondfruit = fruits[1]; // Banana
+//  const thirdfruit = fruits[2]; // Mango
+//  const fourthfruit = fruits[3]; // Oranges
+
+//  console.log(firstfruit);
+//  console.log(secondfruit);
+//  console.log(thirdfruit);
+//  console.log(fourthfruit);
+
+
+
+// 03 example: array indexing for cars
+
+ const cars=['BMW', 'TOYOTA', 'FERRARII', 'LAMBURGINI', 'MITSUBISHI'];
+
+ const firstcar= cars[0]; // BMW
+ const secondcar= cars[1]; // TOYOTA
+ const thirdcar= cars[2]; // FERRARI
+ const fourthcar= cars[3]; // LAMBURGINI
+ const fifthcar= cars[4]; // MITSUBISHI
+
+ console.log(firstcar);
+ console.log(secondcar);
+ console.log(thirdcar);
+ console.log(fourthcar);
+ console.log(fifthcar)
