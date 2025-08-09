688cc95e7c32e36de2f7629c1cdc7d9012db20c4
Muhammad Musanif <muhammadmusanif7292@gmail.com>
2025-08-09 23:42:13 +0500

    Using splice method an array mutability means which specified element you want to remove,add or replace.

---
+++ 03_Arrays/splice_array.js
@@ -0,0 +1,8 @@
+// Splicing in an array is adding,removing, replacing an element to a specified place.
+
+const Vehicals=["BMW", "FERRARI", "COROLLA", "MERCIDIES",];
+
+Vehicals.splice (2, 2, "TOYOTA", "4*4"); // replacing the 3rd element with TOYOTA.
+
+console.log(Vehicals);
+
