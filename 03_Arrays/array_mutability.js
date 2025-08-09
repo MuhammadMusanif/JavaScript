45a6494c2d9000409c69e39bc97a3450bd551b47
Muhammad Musanif <muhammadmusanif7292@gmail.com>
2025-08-09 23:19:27 +0500

    Array mutabiity contain the operations in the array

---
+++ 03_Arrays/array_mutability.js
@@ -0,0 +1,93 @@
+// what is meant by array mutability
+// Array is mutable bracuse of, When we create an array after that we will do some operation like adding element,romving, like we make changes in array.
+
+
+// Array mutability 
+
+// const fruits=["Apple", "Mangoes", "Banana's", "Strawberry"];
+
+// console.log(fruits);
+
+
+// Arrays modification
+
+// const fruits=["Apple", "Mangoes", "Banana's", "Strawberry"];
+
+// fruits[1]= "Orange"; // "Orange" The element on index 1 will be modified.
+
+// console.log(fruits);
+
+
+// Adding element
+
+// const fruits=["Apple", "Mangoes", "Banana's", "Strawberry"];
+
+// fruits[4]= "Mangoes"; // it index 4 element will be added to array "Mangoes".
+
+// console.log(fruits);
+
+
+
+
+// PoP/Removing an elements from array
+
+// const fruits=["Apple", "Mangoes", "Banana's", "Strawberry"];
+
+// fruits.pop(3); // "Apple" element on index 0 will be rempoved
+
+// console.log(fruits);
+
+
+// PUSH/ADD in element to an array.
+
+
+
+// const fruits=["Apple", "Mangoes", "Bananas", "Strawberry"];
+// fruits.push("Kiwi");
+// console.log(fruits);
+
+
+
+// PRACTICE ON PUSH AND POP
+
+// const fruits=["Apple", "Mangoes", "Bananas", "Strawberry"];
+
+// const removedfruits= fruits.pop();
+// console.log("Fruits are:", fruits);
+// console.log("Removed Fruits are:", removedfruits);
+
+
+const students= ["Muhammad","Mustafa","Majeeed","Musanif","Hamza"];
+
+// const addstudents= students.push("Mushtaq","Roman");
+
+// console.log("Array before adding extra students", students);
+
+// console.log("Array after adding an extra students", addstudents);
+
+// console.log('before push', students)
+
+// students.push("Malik majeed")
+
+// console.log('array after push', students)
+// const length= students.pop();
+// console.log(students)
+
+
+// Unshift in an array
+
+
+// const fruits=["Apple", "Banana"];
+
+// fruits.unshift=("Apple","Banana");
+
+// console.log(fruits)
+
+
+
+const fruits=["Apple", "Banana", "Mango"];
+
+fruits.unshift=("Mango");
+
+console.log(fruits);
+
