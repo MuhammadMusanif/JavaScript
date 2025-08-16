bb142d17bdf945492981933afdb2e2d3c1dcd312
Muhammad Musanif <muhammadmusanif7292@gmail.com>
2025-08-16 17:12:32 +0500

    Thus is Java script practicing including Objects, function, operator, Arrays etc.

---
+++ 03_Arrays/first.js
@@ -0,0 +1,41 @@
+// creating an object of name profile
+
+/*const profile= {
+    username: "MalikMajeedZia",
+    followers:50000,
+    following:123,
+    posts:400,
+} 
+console.log(profile);*/
+
+
+// Operator in java scripts like +,-,*,% etc.
+
+
+// Operator +
+let a = 3;
+let b= 4;
+let c= a+b;
+
+console.log("The sum of a + b =",c)
+
+// Operator -
+let d = 9;
+let e= 4;
+let f= d+e;
+
+console.log("The sub of d - e =",f)
+
+//operator *
+let i = 9;
+let h= 4;
+let g= i*h;
+
+console.log("The Mul of i * h =",g)
+
+// Operator %
+let x = 9;
+let y = 4;
+let z = x%y;
+
+console.log("The Mul of i % h =",z)
